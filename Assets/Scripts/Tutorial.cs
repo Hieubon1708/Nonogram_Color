81ee12008a298b4b8c4547e6231d7fb61f5ed546
HieuBon <Hieubon3781@gmail.com>
2024-11-15 00:12:31 +0700

    V

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,25 +1,44 @@
+using TMPro;
 using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
     public GameObject home;
     public GameObject gamePlay;
+    public int step = 1;
+    public string[] content;
+    public GameObject panelStart;
+    public GameObject popupContent;
+    public TextMeshProUGUI textContent;
+    public TextMeshProUGUI textStep;
 
     public void StartTutorial()
     {
-
+        panelStart.SetActive(false);
+        Step();
     }
 
-    public void SkipTutorial()
+    public void Step()
     {
+        if (step < 12)
+        {
+            textContent.text = content[step - 1];
+            textStep.text = "step " + step + "/12";
 
+            popupContent.SetActive(false);
+            popupContent.SetActive(true);
+            step++;
+        }
     }
 
     public void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             StartTutorial();
+        }if (Input.GetKeyDown(KeyCode.S))
+        {
+            Step();
         }
     }
 }
